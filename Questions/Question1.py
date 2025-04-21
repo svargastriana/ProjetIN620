62de91e70dd062afcc67762f4284f79a96cc4b84
César CHARBEY <cesar.charbey@ens.uvsq.fr>
2025-04-21 12:42:50 +0200

    Ajout des makefie par question

--- Questions/Question1.py
+++ Questions/Question1.py
@@ -1,11 +1,16 @@
 import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))) #on doit ajouter le parent du fichier pour accéder à machineturing.py
+from AutomateCellulaire import *
 
 def question1():
     """Fonction qui permet d'éxécuter ce que l'on nous demande pour la question 6
     Question : Proposer une structure de données qui permet de représenter un automate cellulaire. Attention, l’espace d’états des cellules doit être préciser (ça n’est pas forcément {0, 1} comme dans l’exemple ou le jeu de la vie)."""
-    pass
+    automate = Automate_cellulaire(['0', '1'], {}, '_')
+    print("Espace d'états :", automate.espace_etats)
+    print("Transitions :", automate.transitions)
+    print("État par défaut :", automate.etat_par_defaut)
+    print("---------------------------------------------------------")
 
 if __name__ == "__main__":
     question1()
--- Questions/Question3.py
+++ Questions/Question3.py
@@ -1,11 +1,16 @@
 import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))) #on doit ajouter le parent du fichier pour accéder à machineturing.py
+from AutomateCellulaire import *
 
 def question3():
     """Fonction qui permet d'éxécuter ce que l'on nous demande pour la question 6
     Question : Ecrire une fonction qui lit un fichier texte contenant le code d’un automate cellulaire et un mot d’entrée et qui initialise la structure de donn´ees pour représenter cet automate."""
-    pass
+    print("Lecture du fichier règle_110.txt")
+    automate, mot = lire_automate("Automates/règle_110.txt")
+    print("Transitions :", automate.transitions)
+    print("Mot initial :", mot)
+    print("---------------------------------------------------------")
 
 if __name__ == "__main__":
     question3()
